489e8489985a2daeb07220327ffeb4b3c04843a7
James Moultrie <jamesmoultrie@jamess-air.attlocal.net>
2019-10-26 20:08:28 -0400

    put back in db stuff

--- app.js
+++ app.js
@@ -14,56 +14,56 @@ app.use(express.static(__dirname + "/public"));
 //   insecureAuth : true
 // });
 
-// // Set the configuration for your app
-// // TODO: Replace with your app's config object
-// var firebaseConfig = {
-//   apiKey: '<your-api-key>',
-//   authDomain: '<your-auth-domain>',
-//   databaseURL: '<your-database-url>',
-//   storageBucket: '<your-storage-bucket-url>'
-// };
-// firebase.initializeApp(firebaseConfig);
-//
-// // Get a reference to the storage service, which is used to create references in your storage bucket
-// var storage = firebase.storage();
+var connection = mysql.createConnection({
+  host     : 'mysql://bba7090bbe2dde:b1237e3b@us-cdbr-iron-east-05.cleardb.net/heroku_e006ccfc8d8e81b?reconnect=true',
+  user     : 'bba7090bbe2dde',
+  password : 'b1237e3b',
+  insecureAuth : true
+});
 
-// var w = "CREATE DATABASE IF NOT EXISTS join_us"
-// connection.query(w, function(err){
-//   if(err){
-//     throw err;
-//   }
-//   else{
-//     connection.query("USE join_us", function(err){
-//       if(err) throw err;
-//       else{
-//         connection.query("CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(100))", function(err){
-//           if(err) throw err;
-//         });
-//       }
-//     });
-//   }
-// });
+// Set the configuration for your app
+// TODO: Replace with your app's config object
+var firebaseConfig = {
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url>',
+  storageBucket: '<your-storage-bucket-url>'
+};
+firebase.initializeApp(firebaseConfig);
 
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
 
+var w = "CREATE DATABASE IF NOT EXISTS join_us"
+connection.query(w, function(err){
+  if(err){
+    throw err;
+  }
+  else{
+    connection.query("USE join_us", function(err){
+      if(err) throw err;
+      else{
+        connection.query("CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(100))", function(err){
+          if(err) throw err;
+        });
+      }
+    });
+  }
+});
 
-app.get("/", function(req, res){
-    // // Find count of users in DB
-    // var q = "SELECT * FROM users";
-    // connection.query(q, function(err, results){
-    //     if(err) throw err;
-    //
-    //     var count = results.length
-    //     var emails = results
-    //
-    //     res.render("home", {
-    //       count: count,
-    //       emails: emails
-    //     });
-    // });
+app.get("/", function(req, res) {
+    // Find count of users in DB
+    var q = "SELECT * FROM users";
+    connection.query(q, function(err, results){
+        if(err) throw err;
 
-    res.render("home", {
-      count: 123,
-      emails: [{email:'asdf@asdf.com'}]
+        var count = results.length
+        var emails = results
+
+        res.render("home", {
+          count: count,
+          emails: emails
+        });
     });
 });
 
