567363423456093f68baa3b20093d200accbe222
James Moultrie <jamesmoultrie@jamess-air.attlocal.net>
2019-10-26 20:13:51 -0400

    nix firebase

--- app.js
+++ app.js
@@ -21,18 +21,18 @@ var connection = mysql.createConnection({
   insecureAuth : true
 });
 
-// Set the configuration for your app
-// TODO: Replace with your app's config object
-var firebaseConfig = {
-  apiKey: '<your-api-key>',
-  authDomain: '<your-auth-domain>',
-  databaseURL: '<your-database-url>',
-  storageBucket: '<your-storage-bucket-url>'
-};
-firebase.initializeApp(firebaseConfig);
+// // Set the configuration for your app
+// // TODO: Replace with your app's config object
+// var firebaseConfig = {
+//   apiKey: '<your-api-key>',
+//   authDomain: '<your-auth-domain>',
+//   databaseURL: '<your-database-url>',
+//   storageBucket: '<your-storage-bucket-url>'
+// };
+// firebase.initializeApp(firebaseConfig);
 
 // Get a reference to the storage service, which is used to create references in your storage bucket
-var storage = firebase.storage();
+// var storage = firebase.storage();
 
 var w = "CREATE DATABASE IF NOT EXISTS join_us"
 connection.query(w, function(err){
